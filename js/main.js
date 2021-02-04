a0f27d7db43fb491067ff7090847670b54041992
viktorevstigneev <viktor_evstigneev14@bk.ru>
2021-02-04 15:00:34 +0300

    calendar-events/TASK-1:add loop event function

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const NUMBER_OF_MILLISECONDS_DAY = 86400000;
-const MAX_TIMEOUT_MILLISECONDS = 2147483647;
+const MAX_DELAY_MILLISECONDS = 2147483647;
+const MIN_DELAY_MILLISECONDS = 0;
+const NUMBER_OF_DAYS_WEEK = 7;
 
 const Period = {
 	DAY: "Day",
@@ -51,25 +53,46 @@ const getDifferenceBetweenDates = (strartDate, finalDate) => {
 	return datesIntervalList;
 }
 
-const setLongTimeout = (callback, delay) => {
-	if (delay > MAX_TIMEOUT_MILLISECONDS) {
-			setTimeout(() => {
-				setLongTimeout(callback, (delay - MAX_TIMEOUT_MILLISECONDS)); 
-			}, MAX_TIMEOUT_MILLISECONDS);
-	}
-	else{ 
-		setTimeout(callback, delay);
-	}
+const getMillisecondsToNextDay = (hours, minutes) => {
+	currentDate = new Date(); 
+	const nextDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate()+1);
+	nextDate.setHours(hours, minutes);
+	const difference = nextDate - currentDate; 
+
+  return difference; 
 }
 
-const createNewEvent = (eventName, eventTime, callback) =>{
+const setDaysTimeout = (callback) => {
+	let dayCount = 0;
+	let timer = setInterval(() => {
+			dayCount++;  
+			if(dayCount === NUMBER_OF_DAYS_WEEK) {
+				 clearInterval(timer);
+				 callback.apply(this,[]);
+			}
+	},NUMBER_OF_MILLISECONDS_DAY);
+}
+
+const setLongTimeout = (callback, delay, eventName) => {
+	if(delay > MAX_DELAY_MILLISECONDS) {   
+  	eventName = setTimeout(() => { 
+				setLongTimeout(callback, (delay - MAX_DELAY_MILLISECONDS), eventObject); 
+			}
+		, MAX_DELAY_MILLISECONDS);
+	}
+ 	else if(delay > MIN_DELAY_MILLISECONDS && delay <= MAX_DELAY_MILLISECONDS) {    
+		eventName = setTimeout(callback, delay);   
+	} 
+}
+
+const createNewEvent = (eventName, eventTime, callback) => {
+	let eventActionName = setLongTimeout(callback, eventTime.getTime() - new Date().getTime(), eventName);
 	const eventObj = {
 		id: getUniqueId(),
 		name: eventName,
 		time: eventTime,
-		action: callback
-	}
-	setLongTimeout(callback, eventTime.getTime() - new Date().getTime());
+		action: eventActionName	
+	} 
 	eventList.push(eventObj);
 }
 
@@ -99,18 +122,24 @@ const changeEventDate = (id, eventTime, newEventTime) => {
 }
 
 const showEventslist = (period, startDate, finalDate) => {
+	let result = [];
+
 	switch (period) {
 		case Period.DAY:
 			eventList.forEach((item) => {
-			  if(startDate.getDay() === item.time.getDay()){
-					console.log(`id: ${item.id}, name: ${item.name}`);
+				if(startDate.getDay() === item.time.getDay()
+					&& startDate.getMonth() === item.time.getMonth()
+					&& startDate.getFullYear() === item.time.getFullYear()
+				){
+					result.push(item);
 				}
 			});
+			console.log("result: ", result);
 			break;
 
 		case Period.WEEK:
 			const currentWeek = getCurrentWeekOfChoosingDay(startDate);
-			const result = eventList.filter((eventItem) => {
+			result = eventList.filter((eventItem) => {
 				return currentWeek.some((date) => {
 					return (eventItem.time.getDate() === date.getDate()
 									&& eventItem.time.getMonth() === date.getMonth()
@@ -123,15 +152,18 @@ const showEventslist = (period, startDate, finalDate) => {
 
 		case Period.MONTH:
 			eventList.forEach((item) => {
-				if(startDate.getMonth() === item.time.getMonth()){
-					console.log(`id: ${item.id}, name: ${item.name}`);
+				if(startDate.getMonth() === item.time.getMonth()
+					&& startDate.getFullYear() === item.time.getFullYear()
+				){
+					result.push(item);
 				}
 			 });
+			 console.log(result);
 			break;
 
 		case Period.INTERVAL:
 			 const intervalDays = getDifferenceBetweenDates(startDate, finalDate);
-			 const events = eventList.filter((eventItem) => {
+			 result = eventList.filter((eventItem) => {
 				return intervalDays.some((date) => {
 					return (eventItem.time.getDate() === date.getDate()
 									&& eventItem.time.getMonth() === date.getMonth()
@@ -139,30 +171,30 @@ const showEventslist = (period, startDate, finalDate) => {
 					);
 				})
 			});
-			console.log(events);
+			console.log(result);
 			break;
 	}
 }
 
-const setCyclicEvent = (eventName, eventTime, callback) =>{
+const setEveryDayEvent = (eventName, callback, hours, minutes) => {
+	let	eventActionName = setInterval(callback, getMillisecondsToNextDay(hours, minutes));
 	const eventObj = {
 		id: getUniqueId(),
 		name: eventName,
-		time: eventTime,
-		action: callback
-	}
-	setLongTimeout(callback, eventTime.getTime() - new Date().getTime());
+		time: "every day",
+		action: eventActionName
+	} 
 	eventList.push(eventObj);
 }
 
-// createNewEvent("buying", new Date(2021, 1, 3, 14, 50), () => {console.log("buy a bread")});
-// createNewEvent("test", new Date(2021, 1, 3, 14, 21), () => {console.log("test")});
-
-// showEventslist("day",new Date(2021, 1, 1));
-// showEventslist("month",new Date(2021, 1, 1));
+// createNewEvent("buying", new Date(2021, 1, 4, 13, 47), () => {console.log("buy a bread")});
+createNewEvent("test", new Date(2021, 2, 4, 14, 58), () => {console.log("test")});
+// console.log('eventList: ', eventList);
+// showEventslist("Day",new Date(2021, 1, 4));
+// showEventslist("Month",new Date(2021, 1, 1));
 // showEventslist("Week",new Date(2021, 1, 1));
 // setInterval(updateСurrentDate, 10);
-// setInterval(checkingDateEvents, 10);
+
 // console.log('eventList: ', eventList);
 // deleteEvent(1,"buying");
 // console.log('eventList: ', eventList);
